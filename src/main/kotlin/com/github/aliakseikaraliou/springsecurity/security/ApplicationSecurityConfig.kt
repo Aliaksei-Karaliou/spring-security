49e76ce5f49c96c20c6fdbba9f2558920d6390ca
Aliaksei Karaliou <aliaksei_karaliou1@epam.com>
2020-09-07 13:40:57 +0300

    3. base user details service (inmemory)

--- src/main/kotlin/com/github/aliakseikaraliou/springsecurity/security/ApplicationSecurityConfig.kt
+++ src/main/kotlin/com/github/aliakseikaraliou/springsecurity/security/ApplicationSecurityConfig.kt
@@ -1,9 +1,13 @@
 package com.github.aliakseikaraliou.springsecurity.security
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 
 @Configuration
 @EnableWebSecurity
@@ -16,5 +20,16 @@ class ApplicationSecurityConfig : WebSecurityConfigurerAdapter() {
                 .authorizeRequests()
                 .antMatchers("/", "index").permitAll()
                 .anyRequest().authenticated()
+    }
+
+    @Bean
+    override fun userDetailsService(): UserDetailsService {
+        val annaSmithUser = User.builder()
+                .username("annasmith")
+                .password("password")
+                .roles("STUDENT") //ROLE_STUDENT
+                .build()
+
+        return InMemoryUserDetailsManager(annaSmithUser)
     }
 }
